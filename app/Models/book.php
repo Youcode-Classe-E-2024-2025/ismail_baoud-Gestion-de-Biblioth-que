68bf52c1e77edfa8a4a715585d5bcd412f301f84
ismailbaoud <ibaoud46@gmail.com>
2025-02-20 15:35:53 -0600

    definer les modeles (book et user et reservation ) et leur proprieter ()

--- app/Models/book.php
+++ app/Models/book.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class book extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'author',
+        'price',
+        'image',
+        'description'
+    ];
     //
 }
